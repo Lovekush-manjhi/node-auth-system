e33fce57ac8241d99a15427ae33f1dab50ce2d20
lovekush <lovekushmanjhi05@gmail.com>
2024-04-06 15:07:43 +0530

    new

---
+++ config/key.js
@@ -0,0 +1,5 @@
+module.exports = {
+  // MongoURI: "mongodb://127.0.0.1/Tests",
+  // MongoURI: 'mongodb+srv://lovekushmanjhi05:9Ag79PoE52QAHYtk@cluster0.wsjggaf.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0'
+  MongoURI:'mongodb+srv://lovekushmanjhi05:FgZTXxKK0zyTKhVm@cluster0.wsjggaf.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0'
+};
